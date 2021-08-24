6736d0d275ec8237085f5a24ea709479e096c5b0
LayanaBhaskaran <layanabhaskaran@gmail.com>
2021-08-24 16:07:53 +0530

    my commit

---
+++ src/component/Red.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Red() {
+    return (
+        <div
+        style={{backgroundColor : 'red',width:'1500px',height:'900px'}}>
+            <h1>Its red...</h1>
+    </div>
+    )
+}
+
+export default Red
